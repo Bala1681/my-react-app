1ff65d3a8f0a361dc8056a1d2425b675db528e13
Balavignesh Sarveshwaran <balavigneshsarveshwaran@AGLS-MLT-469.local>
2022-02-02 12:05:57 +0530

    first react app

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Task from "./components/Task"
+import { Taskhead } from './components/taskhead';
+export default function App() {
+  return (
+    <div>
+      <Taskhead></Taskhead>
+     
+
+    </div>
+    
+  );
+}
+
+
